70a2484afeb6db03386512c1d784fc11f880a441
PETEPEtrek <senatoroff.p@gmail.com>
2024-10-10 17:32:21 +0300

    child process fixed

--- lab1/child.sh
+++ lab1/child.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+date=$(date +"%F")
+startTime=$(date +"%s")
+
 while :
 do 
   read -a memArr <<< "$(cat /proc/meminfo | grep 'MemAvailable')"
-  pidNUM=$(cat "PID.txt")
-  echo "$(date +"%F") $(date +"%T") ${memArr[1]}kB" >> "log${pidNUM}.csv"
-  sleep 86400
+  if [[ date -ne $(date +"%F") ]]
+  then
+	  date=$(date +"%F")
+  fi
+  if [[ ! -e "$date;$startTime.csv" ]]
+  then
+  	printf "%-15s %-15s %-15s\n" "Date" "Monitoring Time" "Memory" > "$date;$startTime.csv" 
+	printf "%-15s %-15s %-15s\n" "$(date +"%F")" "$(date +"%T")" "${memArr[1]}kB" >> "$date;$startTime.csv"
+  else
+	printf "%-15s %-15s %-15s\n" "$(date +"%F")" "$(date +"%T")" "${memArr[1]}kB" >> "$date;$startTime.csv"
+  fi
+  sleep 60
 done
